8cf186c8f3c1e8b81de95dbb34890993470c008f
techjmi <contactshamim62@gmail.com>
2024-12-24 23:48:18 +0530

    key

--- utility/createClient.js
+++ utility/createClient.js
@@ -9,7 +9,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 // console.log(supabaseUrl);
 
 
-const NEXT_PUBLIC_SUPABASE_URL='https://zwrmuyxfudggdbhictbn.supabase.co'
-const NEXT_PUBLIC_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3cm11eXhmdWRnZ2RiaGljdGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MTU1NjQsImV4cCI6MjA1MDE5MTU2NH0.f1cPBHkRME8mmwCg9xK3BDHyvWgP2SIgRc9XwaZyjRw'
+// const NEXT_PUBLIC_SUPABASE_URL='https://zwrmuyxfudggdbhictbn.supabase.co'
+// const NEXT_PUBLIC_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3cm11eXhmdWRnZ2RiaGljdGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MTU1NjQsImV4cCI6MjA1MDE5MTU2NH0.f1cPBHkRME8mmwCg9xK3BDHyvWgP2SIgRc9XwaZyjRw'
             
-export const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl,supabaseKey);
